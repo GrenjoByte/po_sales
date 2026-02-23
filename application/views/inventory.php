d3c0fed10bfe5be999ff0c4613cc7902ad956354
GrenjoByte <grenjo.dev@gmail.com>
2026-02-23 17:21:04 +0800

    Started item cards

--- application/views/inventory.php
+++ application/views/inventory.php
@@ -16,15 +16,26 @@
 			<div class="row mt-3 mb-4 text-center">
 				<h4>Inventory Manager</h4>
 			</div>
-			<div class="row">
+		</div>
+		<div class="container mt-4">
+			<div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4" id="pos_item_cards_container">
 				<div class="col">
-					Column
-				</div>
-				<div class="col">
-					Column
-				</div>
-				<div class="col">
-					Column
+					<div class="card h-100">
+						<div class="card-header d-flex justify-content-center align-items-center" style="height: 120px;">
+							<i class="bi bi-box" style="font-size: 3rem;"></i>
+						</div>
+						<div class="card-body">
+							<figure class="card-title fs-6 fw-light" role="button">
+								<blockquote class="blockquote">
+									<p class="text-truncate overflow-tooltip">Faber-Castell das dasd asd asd asdasd ballpen</p>
+								</blockquote>
+								
+								<figcaption class="blockquote-footer">
+									BO-001
+								</figcaption>
+							</figure>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -32,4 +43,23 @@
 	<footer>
 	</footer>
 </body>
+<script type="text/javascript">
+$(function () {
+
+    $('.overflow-tooltip').each(function () {
+        var $el = $(this);
+        // check if content is actually overflowing
+        if (this.scrollWidth > this.clientWidth) {
+            var full_text = $el.text().trim();
+
+            $el.attr('title', full_text)
+               .attr('data-bs-toggle', 'tooltip')
+               .attr('data-bs-placement', 'top');
+
+            new bootstrap.Tooltip(this);
+        }
+    });
+
+});
+</script>
 </html>
