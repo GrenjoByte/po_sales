4c14e9a9a48717283e9e74b1e91395e8821792a5
GrenjoByte <grenjo.dev@gmail.com>
2026-02-24 23:04:03 +0800

    Item card finalized
    Item card finalized and initial cart item design started

--- application/views/inventory.php
+++ application/views/inventory.php
@@ -4,6 +4,31 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Inventory Management</title>
+	<style>
+		.img-hover-wrapper .hover-dimmer {
+			background-color: rgba(0,0,0,0); /* fully transparent initially */
+			backdrop-filter: blur(0px);
+			-webkit-backdrop-filter: blur(0px);
+			transition: all .2s ease-in-out;
+		}
+
+		.img-hover-wrapper:hover .hover-dimmer {
+			background-color: rgba(0,0,0,0.35); /* dimmer color */
+			backdrop-filter: blur(1px);
+			-webkit-backdrop-filter: blur(1px);
+		}
+
+		.img-hover-wrapper .hover-btn {
+			opacity: 0;
+			transform: scale(0.9);
+			transition: all .2s ease-in-out;
+		}
+
+		.img-hover-wrapper:hover .hover-btn {
+			opacity: 1;
+			transform: scale(1);
+		}
+	</style>
 </head>
 <body>
 	<header>
@@ -21,16 +46,30 @@
 			<div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4" id="pos_item_cards_container">
 				<div class="col">
 					<div class="card h-100">
-						<div class="card-header d-flex justify-content-center align-items-center" style="height: 120px;">
-							<i class="bi bi-box" role="button" style="font-size: 3rem;"></i>
+						<div class="position-relative img-hover-wrapper">
+							<img src="<?php echo base_url();?>photos/pos_images/c2_apple_1l.jpg"
+							class="card-img-top"
+							alt="product_image"
+							style="aspect-ratio:5/3;object-fit:contain;background-color:#edf1f4;">
+
+							<!-- Dimmer overlay -->
+							<div class="position-absolute top-0 start-0 w-100 h-100 hover-dimmer d-flex justify-content-center align-items-center">
+								<small class="btn btn-secondary btn-sm hover-btn" role="button">
+									Modify
+								</small>
+							</div>
 						</div>
 						<small class="card-body">
 							<div class="card-title fs-6 text-truncate overflow-tooltip" role="button">
 								Faber-Castell das dasd asd asd asdasd ballpen
 							</div>
 							<small class="card-subtitle text-muted d-block text-truncate">
-								Item Code: BO-001
+								BO-001
 							</small>
+							<div class="d-flex fs-6 mt-1 fw-semibold align-items-center gap-2">
+								<span>20</span>
+								<span>pcs</span>
+							</div>
 						</small>
 						<div class="card-footer">
 							<div class="d-flex justify-content-start align-items-center">
